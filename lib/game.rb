f70550ff3935ddc6166a837d3445e94f84321e7b
CodeRed30 <katrinaharradine@gmail.com>
2021-01-22 15:52:52 +0000

    Skinny controllers

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class Game
-  def attack(player)
-    player.receive_damage
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def attack
+    @player_2.receive_damage
   end
 end
